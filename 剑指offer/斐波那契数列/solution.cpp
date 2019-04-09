6e0b3a3bcadc5c3e40196283f48b625dc2b99f95
yangjie <2212371178@qq.com>
2019-04-09 13:12:23 +0800

    Update solution.cpp

--- 剑指offer/斐波那契数列/solution.cpp
+++ 剑指offer/斐波那契数列/solution.cpp
@@ -1,8 +1,25 @@
+//递归做法
 class Solution {
 public:
     int Fibonacci(int n) {
        if(n==0) return 0;
        else  if(n<=2) return 1;
        else return Fibonacci(n-1)+Fibonacci(n-2);
+    }
+};
+//保存中间项做法
+class Solution {
+public:
+    int Fibonacci(int n) {
+       if(n==0) return 0;
+       if(n==1) return 1;
+       int first=1,second=0,ans=0;
+       for(int i=2;i<=n;i++)
+       {
+           ans=first+second;
+           second=first;
+           first=ans;
+       }
+       return ans;
     }
 };

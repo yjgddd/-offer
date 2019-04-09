9e47b837a773f8d9eec488a9464387f9c33f89dc
yangjie <2212371178@qq.com>
2019-04-09 13:31:15 +0800

    Update solution.cpp

--- 剑指offer/跳台阶/solution.cpp
+++ 剑指offer/跳台阶/solution.cpp
@@ -1,3 +1,4 @@
+//递归
 class Solution {
 public:
     int jumpFloor(int number) {
@@ -5,4 +6,21 @@ public:
         else if(number==2) return 2;
         else return jumpFloor(number-1)+jumpFloor(number-2);
     }
+};
+//迭代
+class Solution {
+public:
+    int jumpFloor(int number) {
+       if(number==0) return 0;
+       if(number==1) return 1;
+       if(number==2) return 2;
+       int first=1,second=2,ans=0;
+       for(int i=3;i<=number;i++)
+       {
+           ans=first+second;
+           first=second;
+           second=ans;
+       }
+       return ans;
+    };
 };

0d37a7e57e8ce4261523d19ac96dd9f1d1b424e9
yangjie <2212371178@qq.com>
2019-05-04 21:32:39 +0800

    Create solution.cpp

---
+++ 剑指offer/合并两个排序的链表/solution.cpp
@@ -0,0 +1,27 @@
+/*
+struct ListNode {
+	int val;
+	struct ListNode *next;
+	ListNode(int x) :
+			val(x), next(NULL) {
+	}
+};*/
+class Solution {
+public:
+    ListNode* Merge(ListNode* pHead1, ListNode* pHead2)
+    {
+        if(pHead1==NULL) return pHead2;
+        else if(pHead2==NULL) return pHead1;
+        else if(pHead1->val<pHead2->val)
+        {
+            pHead1->next= Merge(pHead1->next,pHead2);
+            return pHead1;
+        }
+        else
+        {
+            pHead2->next= Merge(pHead1,pHead2->next);
+            return pHead2;
+        }
+        
+    }
+};

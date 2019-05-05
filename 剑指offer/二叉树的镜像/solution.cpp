2165564977ff45fbb10f8a68066ea1f3c4cc9393
yangjie <2212371178@qq.com>
2019-05-05 17:05:16 +0800

    Create solution.cpp

---
+++ 剑指offer/二叉树的镜像/solution.cpp
@@ -0,0 +1,21 @@
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+       if(pRoot==NULL) return;
+       if(pRoot->left==NULL&&pRoot->right==NULL) return;
+        TreeNode *pTemp=pRoot->left;
+        pRoot->left=pRoot->right;
+        pRoot->right=pTemp;
+        if(pRoot->left) Mirror(pRoot->left);
+        if(pRoot->right) Mirror(pRoot->right);
+    }
+};
